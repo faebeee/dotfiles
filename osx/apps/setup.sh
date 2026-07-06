c9cfab807421ef587883d59d8a188ff9683b90b1
fabs <faebeee@gmail.com>
2026-07-06 13:21:20 +0200

    Improve osx install

---
+++ osx/apps/setup.sh
@@ -0,0 +1,14 @@
+installApp() {
+  APP=$1
+  if ! [ -x "$(command -v $APP)" ]; then
+    echo "$APP not found. Start install..."
+    brew install $APP
+  fi
+}
+
+echo "####################"
+echo "  INSTALLING APPS "
+echo "####################"
+
+installApp fzf
+installApp lsd
---
+++ shared/wallpapers/setup.sh
@@ -0,0 +1,1 @@
+ln -sf "$(pwd -P)/wallpapers" $HOME/Pictures/wallpapers

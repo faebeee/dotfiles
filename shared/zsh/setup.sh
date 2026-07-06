1461a2becebe0b8869aa5426526831fcef38e71e
fabs <faebeee@gmail.com>
2026-07-06 13:30:49 +0200

    Add setup scripts

--- shared/zsh/setup.sh
+++ shared/zsh/setup.sh
@@ -1,2 +1,12 @@
 mv "$HOME/.zshrc" "$HOME/.zshrc.bkp"
 cp "$(pwd -P)/.zshrc" "$HOME/.zshrc"
+
+echo "################"
+echo "Installing zsh-autosuggestions"
+echo "################"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+echo "################"
+echo "Installing zsh-syntax-highlighting"
+echo "################"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting

5736b3e9dce53e596f26a46517f2901ca72e761a
faebeee <faebeee@gmail.com>
2026-07-09 11:07:23 +0200

    Add DMS config

--- linux/dms/setup.sh
+++ linux/dms/setup.sh
@@ -1,1 +1,7 @@
-sudo -v && curl -fsSL https://install.danklinux.com | sh -s -- -c hyperland -t ghostty -y
+if [ ! -d ~/.config/DankMaterialShell ]; then
+  sudo -v && curl -fsSL https://install.danklinux.com | sh -s -- -c hyperland -t ghostty -y
+fi
+
+mv "$HOME/.config/DankMaterialShell/" "$HOME/.config/backup_DankMaterialShell/"
+
+ln -sf "$(pwd -P)/DankMaterialShell" $HOME/.config/DankMaterialShell

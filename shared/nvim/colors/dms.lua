dc0f896295a4555ee847ca4308efd9910247e7d0
faebeee <faebeee@gmail.com>
2026-07-26 10:31:18 +0200

    Add wallpaper

--- shared/nvim/colors/dms.lua
+++ shared/nvim/colors/dms.lua
@@ -82,9 +82,9 @@ end
 
 if not base46.theme_tables[theme_name] or base46.theme_tables[theme_name].type ~= vim.o.background then
 	local builtin = vim.deepcopy(assert(base46.get_builtin_theme(theme_base)))
-	local harmonized = base46.theme_harmonize(builtin, "#245fcb", harmony)
+	local harmonized = base46.theme_harmonize(builtin, "#fbe3ca", harmony)
 	if settings.matugenTemplateNeovimSetBackground ~= false then
-		harmonized = base46.theme_set_bg(harmonized, "#11131a")
+		harmonized = base46.theme_set_bg(harmonized, "#151311")
 	end
 
 	base46.theme_tables[theme_name] = harmonized

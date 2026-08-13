a807f0ababee618b2bc363f3cf2afb7670ffc2d2
fabs <faebeee@gmail.com>
2026-08-13 11:31:53 +0200

    Update tmux conf

--- shared/tmux/setup.sh
+++ shared/tmux/setup.sh
@@ -1,5 +1,10 @@
+if [ ! -d ~/.tmux ]; then
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+  git clone git@github.com:2KAbhishek/tmux2k.git ~/.tmux/plugins/tmux2k
+fi
+
 rm -rf "$HOME/.tmux.conf"
-cp "$(pwd -P)/.tmux.conf" "$HOME/.tmux.conf"
+ln -sf "$(pwd -P)/.tmux.conf" "$HOME/.tmux.conf"
 
 #rm -rf "$HOME/.tmux.conf"
 #ln -sf "$(pwd -P)/.tmux.conf" $HOME/.tmux.conf

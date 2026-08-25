95eccb770439229a91d44afa283368b51594c99a
faebeee <faebeee@gmail.com>
2026-08-25 16:32:30 +0200

    Update noctalia setup

--- linux/noctalia-hypr/setup.sh
+++ linux/noctalia-hypr/setup.sh
@@ -4,6 +4,9 @@ ln -sf "$(pwd -P)/hypr" $HOME/.config/hypr
 rm $HOME/.config/noctalia/config.toml
 ln -sf "$(pwd -P)/noctalia/config.toml" $HOME/.config/noctalia/config.toml
 
+rm -rf $HOME/.config/noctalia/palettes/
+ln -sf "$(pwd -P)/noctalia/palettes" $HOME/.config/noctalia/palettes
+
 rm $HOME/.local/state/noctalia/settings.toml
 cp "$(pwd -P)/noctalia/settings.toml" $HOME/.local/state/noctalia/settings.toml
 
